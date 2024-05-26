8841b78db8a915dd50c46829d179e4d8ae465978
Lum Hoxha <116526825+lumii06@users.noreply.github.com>
2024-05-26 14:13:52 +0200

    footer and header seperated ADDED

---
+++ Restaurant/Restaurant-main/footer.php
@@ -0,0 +1,24 @@
+<footer class="footer">
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-col">
+                <p>40 Park Ave, Brooklyn, New York</p>
+                <p>1-800-111-2222</p>
+                <p>contact@example.com</p>
+                <div class="footer-socials">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-content">
+            <h1>LaTulipe</h1>
+        </div>
+        <div class="footer-content">
+            <p>Monday - Friday: 9AM - 9PM</p>
+            <p>Saturday: 9AM - 11PM</p>
+            <p>Sunday: 9AM - 11PM</p>
+            <p>Happy Hours: 9AM - 12AM</p>
+        </div>
+    </div>
+</footer>
---
+++ Restaurant/Restaurant-main/header.php
@@ -0,0 +1,47 @@
+<?php
+var_dump($_SESSION); // Debug line
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LaTulipe</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://kit.fontawesome.com/1506ca5daa.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="nav row" id="nav">
+            <h1 class="header-title">LaTulipe</h1>
+
+            <a href="javascript:void(0);" onclick="displayMenu()" class="menu-mobile" id="menu-mobile">
+                <i class="fa-solid fa-bars"></i>
+            </a>
+            <ul class="nav-list row" id="nav-list">
+                <li class="nav-item"><a href="javascript:void(0);" onclick="removeMenu()" class="nav-link"><i class="fa-solid fa-xmark"></i></a></li>
+                <li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="menu.php" class="nav-link">Menu</a></li>
+                <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+                <li class="nav-item"><a href="delivery.php" class="nav-link">Delivery</a></li>
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    
+                    <li class="nav-item"><a href="profile.php" class="nav-link">Profile</a></li>
+                    <li class="nav-item"><a href="logout.php" class="nav-link">Log Out</a></li>
+                <?php else: ?>
+                    <li class="nav-item"><a href="login.php" class="nav-link">Log In</a></li>
+                    <li class="nav-item"><a href="register.php" class="nav-link">Register</a></li>
+                <?php endif; ?>
+                <li class="nav-item"><a href="booknow.php" class="nav-link btn">Book now</a></li>
+            </ul>
+        </nav>
+    </header>
